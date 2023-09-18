89e9b8e57ad88ef879a79620a3a2ca61896ee58e
Vedant Jadhav <vedantjadhav261999@gmail.com>
2023-09-18 11:30:53 +0530

    Added Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,37 +7,42 @@ const Footer = () => {
   return (
     <div id="works" className=" mx-auto m-auto h-[300px]  mt-16 sm:h-[250px]">
       <div className=" bg-yellow-400 h-full flex flex-col gap-8 items-center justify-between p-10 sm:p-7">
-        <h2 
-        data-aos="zoom-out"
-         className=" font-bold text-5xl sm:text-3xl">Let's Talk</h2>
+        <h2 data-aos="zoom-out" className=" font-bold text-5xl sm:text-3xl">
+          Let's Talk
+        </h2>
         <div className=" flex items-center justify-center gap-8 ">
-          <a 
-          data-aos="fade-up" data-aos-duration="1000"
-
+          <a
+            data-aos="fade-up"
+            data-aos-duration="1000"
             href="https://t.me/Jadhav_Vedant"
             className="box font-medium text-white   flex items-center justify-center flex-col"
           >
             <FaTelegramPlane className=" text-black text-3xl hover:scale-125 cursor-pointer" />
             <p>Telegram</p>
           </a>
-          <a 
-          data-aos="fade-up" data-aos-duration="1200"
-
+          <a
+            data-aos="fade-up"
+            data-aos-duration="1200"
             href="https://api.whatsapp.com/send/?phone=9579703839&text&type=phone_number&app_absent=0"
             className="box font-medium text-white  flex items-center justify-center flex-col"
           >
             <IoLogoWhatsapp className=" text-black text-3xl hover:scale-125 cursor-pointer" />
             <p>WhatsApp</p>
           </a>
-          <a 
-          data-aos="fade-up" data-aos-duration="1400"
-
+          <a
+            data-aos="fade-up"
+            data-aos-duration="1400"
             href="https://www.instagram.com/vedantj727/"
             className="box font-medium text-white  flex items-center justify-center flex-col"
           >
             <RiInstagramFill className=" text-black text-3xl hover:scale-125 cursor-pointer" />
             <p>Instagram</p>
           </a>
+        </div>
+        <div className="lg:text-[15px]">
+          Made with
+          <span className="animate-pulse"> ❤️ </span>
+          by Vedant Jadhav
         </div>
       </div>
     </div>
